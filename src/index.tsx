8a515d3221c6ee82947958f273641bb381acf61d
nikolchaa <nikolaivanrandjelovic@gmail.com>
2025-06-10 15:25:25 +0200

    feat: update dependencies and add Tailwind CSS integration ✨
    - Added new dependencies: @radix-ui/react-slot, @tailwindcss/vite, class-variance-authority, clsx, lucide-react, tailwind-merge, and tailwindcss.
    - Removed vite.svg and preact.svg assets.
    - Added new Logo.svg asset.
    - Added shadcn/ui.
    - Introduced Figtree font files for various styles.
    - Refactored style.css to include Tailwind CSS and Figtree font integration.
    - Enhanced tsconfig.json with path aliases for easier imports.
    - Updated vite.config.ts to include Tailwind CSS and SVGR plugin.

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,20 @@
-import { hydrate, prerender as ssr } from 'preact-iso';
+import { hydrate, prerender as ssr } from "preact-iso";
 
-import preactLogo from './assets/preact.svg';
-import './style.css';
+import "./style.css";
+import { Button } from "./components/ui/button";
 
 export function App() {
-	return (
-		<div>
-			<a href="https://preactjs.com" target="_blank">
-				<img src={preactLogo} alt="Preact logo" height="160" width="160" />
-			</a>
-			<h1>Get Started building Vite-powered Preact Apps </h1>
-			<section>
-				<Resource
-					title="Learn Preact"
-					description="If you're new to Preact, try the interactive tutorial to learn important concepts"
-					href="https://preactjs.com/tutorial"
-				/>
-				<Resource
-					title="Differences to React"
-					description="If you're coming from React, you may want to check out our docs to see where Preact differs"
-					href="https://preactjs.com/guide/v10/differences-to-react"
-				/>
-				<Resource
-					title="Learn Vite"
-					description="To learn more about Vite and how you can customize it to fit your needs, take a look at their excellent documentation"
-					href="https://vitejs.dev"
-				/>
-			</section>
-		</div>
-	);
+  return (
+    <div className={"flex h-screen items-center justify-center"}>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
 
-function Resource(props) {
-	return (
-		<a href={props.href} target="_blank" class="resource">
-			<h2>{props.title}</h2>
-			<p>{props.description}</p>
-		</a>
-	);
-}
-
-if (typeof window !== 'undefined') {
-	hydrate(<App />, document.getElementById('app'));
+if (typeof window !== "undefined") {
+  hydrate(<App />, document.getElementById("app"));
 }
 
 export async function prerender(data) {
-	return await ssr(<App {...data} />);
+  return await ssr(<App {...data} />);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-			},
-		}),
-	],
+  plugins: [
+    preact({
+      prerender: {
+        enabled: true,
+        renderTarget: "#app",
+      },
+    }),
+    tailwindcss(),
+    svgr(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
